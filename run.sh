b6184a0235266581ce05c8bf19d16138a55afcb6
manifestswitch <EbXza@yahoo.com>
2013-06-16 18:38:56 +0100

    use global "node" program, not local copy

--- run.sh
+++ run.sh
@@ -12,9 +12,9 @@ touch /tmp/applog
 tailf -n 0 /tmp/applog &
 tailf_pid=$!
 
-./tools/node-v0.10.5-linux-x64/bin/node target/frontserv.jsmacro.js &
+node target/frontserv.jsmacro.js &
 frontserv_pid=$!
-./tools/node-v0.10.5-linux-x64/bin/node target/ui.jsmacro.js &
+node target/ui.jsmacro.js &
 ui_pid=$!
 
 printf 'running\n' >&2
