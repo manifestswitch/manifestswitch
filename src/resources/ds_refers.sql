adafeed4b4bb7ea9df369c6ff946781894d904c8
manifestswitch <EbXza@yahoo.com>
2013-07-17 20:59:27 +0100

    add more tables for new Read/Write key scheme

--- src/resources/ds_refers.sql
+++ src/resources/ds_refers.sql
@@ -28,3 +28,48 @@ CREATE INDEX refers_pkey ON refers (pkey);
 --CREATE INDEX refers_referrer ON refers (referrer);
 -- SELECT .. WHERE referree="x"
 CREATE INDEX refers_referree ON refers (referree);
+
+
+-- New channel mechanism, based on a secret token value and a public
+-- value derived from it using sha256.
+-- In future could be sharded based on some of the sha256 value.
+
+
+-- Just deduplicates the read_key text
+CREATE TABLE read_keys (
+       pkey serial, -- PRIMARY KEY;
+       read_key text
+);
+CREATE INDEX read_keys_pkey ON read_keys (pkey);
+CREATE INDEX read_keys_read_key ON read_keys (read_key);
+
+-- content can be posted to multiple channels
+CREATE TABLE channel_content (
+       pkey serial, -- PRIMARY KEY;
+       read_key integer,
+       hash integer
+);
+CREATE INDEX channel_content_pkey ON channel_content (pkey);
+CREATE INDEX channel_content_read_key ON channel_content (read_key);
+
+-- The initial setup allows us to assign a random Write key as the
+-- pair for a fingerprint Read key
+-- Map from write_key -> fingerprint
+-- (as opposed to the normal map of sha256)
+CREATE TABLE fingerprint_alias (
+       pkey serial, -- PRIMARY KEY;
+       write_key text,
+       fingerprint text
+);
+CREATE INDEX fingerprint_alias_pkey ON fingerprint_alias (pkey);
+CREATE INDEX fingerprint_alias_write_key ON fingerprint_alias (write_key);
+
+-- as above, except for looking up by fingerprint. This uses a
+-- completely different mechanism, so is kept separate.
+CREATE TABLE fingerprint_content (
+       pkey serial, -- PRIMARY KEY;
+       fingerprint_alias integer,
+       hash integer
+);
+CREATE INDEX fingerprint_content_pkey ON fingerprint_content (pkey);
+CREATE INDEX fingerprint_content_fingerprint_alias ON fingerprint_content (fingerprint_alias);
