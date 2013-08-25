61c1d6064832507b745962f9e831ac2cfad792b1
manifestswitch <EbXza@yahoo.com>
2013-08-25 13:18:28 +0100

    add some initial js

--- src/resources/ui-server.js
+++ src/resources/ui-server.js
@@ -29,8 +29,25 @@ function ajaxify_reply_form() {
 
 }
 
+function add_stuff_to_grouproots() {
+    var lis = document.getElementsByTagName('li');
+    for (var i = 0, len = lis.length; i < len; ++i) {
+        lis[i].innerHTML += ' <a href="https://127.0.0.1:8443/posts?parent=' + lis[i].textContent + '">comments</a>';
+    }
+}
 
 function document_onload() {
+    var x = false;
+    $('li').each(function () {
+        var that = this;
+        $(this).find('a').each(function () {
+            $.getJSON('/post/' + this.textContent, function (data) {
+                if (!x) alert(data);
+                x = false;
+            });
+        });
+    })
+
     alert(5);
 }
 
